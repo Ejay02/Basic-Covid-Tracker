c5a110c13790e36765f23ec1ba8392d6eeb9cbb4
Labaks <e.jae02@gmail.com>
2021-10-13 08:23:37 +0100

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fas fa-viruses"></i>
+      <img
+        src="https://img.icons8.com/external-justicon-lineal-justicon/64/000000/external-virus-virus-transmission-justicon-lineal-justicon-1.png"
+      />
+      Covid -19 Tracker
+    </div>
+    <p>
+      API by
+      <a class="text-blue-200" href="https://covid19api.com" target="_blank"
+        >covid19api.com</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
